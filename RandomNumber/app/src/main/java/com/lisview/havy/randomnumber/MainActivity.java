ac718fd18b219b1c6b1a0e8a87577598b5747f2e
Huynh Thi Ha Vy <huynhthihavy12@gmail.com>
2018-10-24 22:39:28 +0700

    Tuan 1: Random Number Commit

---
+++ RandomNumber/app/src/main/java/com/lisview/havy/randomnumber/MainActivity.java
@@ -0,0 +1,33 @@
+package com.lisview.havy.randomnumber;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView Text_number;
+    Button button_random;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Text_number = (TextView) findViewById(R.id.id_number);
+        button_random = (Button) findViewById(R.id.id_button);
+
+        button_random.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // random
+                Random Rnumber = new Random();
+                int iNum = Rnumber.nextInt();
+                Text_number.setText(iNum + "");
+            }
+        });
+    }
+}
